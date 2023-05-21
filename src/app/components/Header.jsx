16531425936dc3549a5e9ab2c196e3e53526d5d1
mubashircoding <mubashirkaiser2009@gmail.com>
2023-05-21 16:12:42 -0400

    Resized the Logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from "@/app/styles/navbar.module.css"
 import Link from "next/link";
 import Image from "next/image";
+import Logo from '../../../public/logo.png'
 import Nav from './Nav';
 const Header = () => {
     return (
         <header className={styles.main_header}>
             <div className={styles.navbar_brand}>
                 <Link href='/'>
-                    <Image src="/logo.png" alt='my logo image' width={150} height={150}/>
+                    <Image src={Logo} alt='my logo image' width={70} height={70}/>
                 </Link>
             </div>
             <Nav/>
